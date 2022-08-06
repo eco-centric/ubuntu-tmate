2313b1d3ea48fdb7b81f73a8b046db3ed37add8c
eco-centric <110648647+eco-centric@users.noreply.github.com>
2022-08-06 10:25:16 -0700

    Update tmate.sh

--- tmate.sh
+++ tmate.sh
@@ -1,1 +1,1 @@
-pip install colabcode && wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip -o ngrok-stable-linux-amd64.zip && ./ngrok authtoken 2Ba9NSUGF6tL3Kkuy0hQhosi7Gf_2kDbhXA4rkcQgLBvpd1u8 && colabcode [-h] --port 10000 [--password    ]
+pip install colabcode && wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip -o ngrok-stable-linux-amd64.zip && ./ngrok authtoken 2Ba9NSUGF6tL3Kkuy0hQhosi7Gf_2kDbhXA4rkcQgLBvpd1u8 && colabcode --port 10000
