23a96e335b35c29cd8194a1b65b046b1e14abae8
eco-centric <110648647+eco-centric@users.noreply.github.com>
2022-08-06 10:37:39 -0700

    Create vs.sh

---
+++ vs.sh
@@ -0,0 +1,1 @@
+pip install colabcode >/dev/null 2>&1 && from colabcode import ColabCode && authtoken ='2Ba9NSUGF6tL3Kkuy0hQhosi7Gf_2kDbhXA4rkcQgLBvpd1u8' && ColabCode(password="123456", authtoken=authtoken)
